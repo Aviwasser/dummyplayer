39914bf4dd66f1a3d09fbc990f12994c0a648fcf
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-05-24 00:16:22 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -14,14 +14,12 @@ public:
 
 	void setChar(char c) { myChar = c; }
 	char getChar() const { return myChar; }
-
-	/**
-	 *  Returns the next move, given the current board.
-	 */
-	virtual const Coordinate play(const Board& board) = 0; 
-
 	/**
 	 *  Returns the name of the current player.
 	 */
 	virtual const std::string name() const = 0; 
+	/**
+	 *  Returns the next move, given the current board.
+	 */
+	virtual const Coordinate play(const Board& board) = 0; 
 };
