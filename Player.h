3075c01b9a50a389df16ce9b87fa7dbf3113eceb
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-05-24 00:00:35 +0300

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Coordinate.h"
+#include "Board.h"
+#include <string>
+
+/**
+ * An abstract player.
+ */
+class Player {
+protected:
+	char myChar;
+public:
+
+	void setChar(char c) { myChar = c; }
+	char getChar() const { return myChar; }
+
+	/**
+	 *  Returns the next move, given the current board.
+	 */
+	virtual const Coordinate play(const Board& board) = 0; 
+
+	/**
+	 *  Returns the name of the current player.
+	 */
+	virtual const std::string name() const = 0; 
+};
