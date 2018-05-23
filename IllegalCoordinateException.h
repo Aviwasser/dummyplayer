235bf6e62cab27db884634daef3bbee32a9b6912
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-05-23 23:59:24 +0300

    Create IllegalCoordinateException.h

---
+++ IllegalCoordinateException.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Coordinate.h"
+#include <string>
+using namespace std;
+
+class IllegalCoordinateException {
+	Coordinate c;
+public:
+	IllegalCoordinateException(Coordinate c): c(c) {}
+	Coordinate theCoordinate() const { return c; }
+};
